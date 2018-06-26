c47cfca61409822511338e865ac24e0370041681
Michaela Patakova <patakovamichala@gmail.com>
2018-06-26 22:04:53 +0200

    7. Zjednodušení algoritmu, který vytvoří body podél linie

--- test.py
+++ test.py
@@ -4,62 +4,25 @@ from shapely.geometry import LineString, Point, shape, mapping
 from pprint import pprint
 from fiona.crs import from_epsg
 
-def prepareLine(line, lenghtOfSegment):
-    points = list(line.coords)
-    previousPoint = None
-    outputPoints = []
-    for p in points:
-        point = Point(p[0], p[1])
-        outputPoints.append(point)
-        if previousPoint is not None:
-            dst = previousPoint.distance(point)
-            if dst >= lenghtOfSegment:
-                additionsCnt = int(dst/lenghtOfSegment)
-                dX = point.x - previousPoint.x
-                dY = point.y - previousPoint.y
-                additionsX = dX / additionsCnt
-                additionsY = dY / additionsCnt
-                for i in range (0, additionsCnt):
-                    newX = previousPoint.x + additionsX*(i+1)
-                    newY = previousPoint.y + additionsY*(i+1)
-                    outputPoints.append(Point(newX,newY))
-        previousPoint = point
-    return outputPoints
-
-
-def cutLine(line, lenghtOfSegment):
+def interpolatePoints(line, lenghtOfSegment):
 
     # preparations
 
     # distance counter
     tmpDistance = 0
 
-    # point n-1
-    previousPoint = None
-
     # array of output points
     outputPoints = []
 
-    # iterate through all points
-    for point in line:
-        # 1. increase tmpDistance and set previous point
-        if previousPoint is None:
-            previousPoint = point
-            outputPoints.append(point)
-        else:
-            dst = previousPoint.distance(point)
-            tmpDistance = tmpDistance + dst
-            previousPoint = point
+    length = line.length
 
-            # 2. add point to output list, if my limit was exceeded
-            if tmpDistance >= lenghtOfSegment:
-                outputPoints.append(point)
-                tmpDistance=0
+    point = None
+    while tmpDistance <= length:
+        point = line.interpolate(tmpDistance)
+        outputPoints.append(point)
+        tmpDistance += lenghtOfSegment  # == tmpDistance = tmpDistance + lengthOfSegment
 
-
-    # add last vertices to the output points
-    verticesCount = len(line)
-    outputPoints.append(line[verticesCount-1])
+    outputPoints.append(point)
     return outputPoints
 
 
@@ -70,7 +33,7 @@ raster = rasterio.open(raster_loc)
 vector_loc = r"C:\Users\patmic\Desktop\CVZ_CVUT\Free_software_gis\Data\silnice_clip_krnapII.shp"
 vector = fiona.open(vector_loc, "r")
 
-lenghtOfSegment = 5
+lenghtOfSegment = 15
 
 vector_output = r"C:\Users\patmic\Desktop\CVZ_CVUT\Free_software_gis\Data\silnice_clip_krnapII_output.shp"
 schema={
@@ -85,8 +48,7 @@ with fiona.open(vector_output, 'w',crs=from_epsg(5514),driver='ESRI Shapefile', 
 
         # there are some geometries of type MultiLineString, which i am throwing away for now
         if line.type == 'LineString':
-            preparedLine = prepareLine(line,1)
-            points = cutLine(preparedLine, lenghtOfSegment)
+            points = interpolatePoints(line, lenghtOfSegment)
             for point in points:
                 prop = {'X': float(point.x), 'Y': float(point.y)}
                 output.write({'geometry': mapping(point), 'properties': prop})
